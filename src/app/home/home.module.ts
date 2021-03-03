7f00428c30c712c292e81765b47644230744d4b4
Ben Ng <ben.ng@outlook.com>
2021-03-03 14:26:13 -0500

    added newsfeed component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, NewsfeedComponent],
   imports: [
     CommonModule,
     SharedModule
